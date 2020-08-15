55af208d40035cc3498cc155473153f9f0958f9a
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-14 17:56:35 -0700

    Added README scaffolding.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,59 @@
 // function to generate markdown for README
-function generateMarkdown({title, description, toc, }) {
+function generateMarkdown({title, description, installation, usage, license, contribution, tests, username, email}) {
   return `# ${title}
 
-`;
+  ![GitHub](https://img.shields.io/github/license/${username}/${ title })
+
+  ## Description:
+
+  ${ description }
+  
+  
+  
+  ## Table of Contents
+  
+  [TOC]
+  
+  
+  
+  ## Installation
+  
+  ${ installation }
+  
+  
+  
+  ## Usage
+  
+  ${ usage }
+  
+  
+  
+  ## Contribution Guidelines
+  
+  ${ contribution }
+  
+  
+  
+  ## Tests
+  
+  ${ tests }
+  
+  
+  
+  ## Questions
+  
+  Please address questions to ${ username }.
+  
+  [Link to Profile](https://github.com/${ username })
+  
+  [E-mail](${ email })
+  
+  
+  
+  ## License
+  
+  ${ license }
+  `;
 }
 
 module.exports = generateMarkdown;
